6af1488aa92f286d6b293abee0fa8f3349cb9d03
martinecormier6 <Martine.Cormier6@tpsgc-pwgsc.gc.ca>
2026-04-27 11:15:56 -0400

    updated date

--- main.js
+++ main.js
@@ -11,7 +11,7 @@
       mm = String(today.getMonth() + 1).padStart(2, "0"),
       yyyy = today.getFullYear(),
       nextYear = yyyy + 1,
-      dayMonth = "20/05/",
+      dayMonth = "05/20/",
       birthday = dayMonth + yyyy;
   
   today = mm + "/" + dd + "/" + yyyy;
